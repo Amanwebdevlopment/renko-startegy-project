d8c219525525a26351624bfb2ba7ab594813c86b
Amanwebdevlopment <Aman.webdevlopment@gmail.com>
2025-07-17 03:52:20 +0530

    Added CORS support

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import strategyRoutes from "./routes/strategyRoutes.js";
+import cors from "cors";
+app.use(cors()); // Allow all origins by default
 
 dotenv.config();
 const app = express();
